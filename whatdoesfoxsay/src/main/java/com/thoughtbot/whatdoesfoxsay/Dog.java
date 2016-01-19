9f6a46316d3bbd3f3a39f93bd69dc014fd511fd2
Steff Kelsey <sk@lavjaveler.com>
2016-01-19 17:09:29 -0500

    Added more sounds to Dog
    Also updated the gradle build to use a Version class to make
    maintaining the version name of the lib easier

--- whatdoesfoxsay/src/main/java/com/thoughtbot/whatdoesfoxsay/Dog.java
+++ whatdoesfoxsay/src/main/java/com/thoughtbot/whatdoesfoxsay/Dog.java
@@ -3,11 +3,16 @@ package com.thoughtbot.whatdoesfoxsay;
 import java.util.List;
 
 /**
- * The Dog goes woof
+ * The Dog goes woof, bow-wow, arf, and bark
  */
 public class Dog extends Animal {
     public Dog() {
-        this("dog", new AlphabeticalListBuilder().with("woof").build());
+        this("dog", new AlphabeticalListBuilder()
+                .with("bow-wow")
+                .with("arf")
+                .with("woof")
+                .with("bark")
+                .build());
     }
 
     public Dog(String name, List<String> responses) {
